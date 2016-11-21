a3b4e07e9ba51fd38797bd7ce6f1d17f20c67716
Vishal Dara <Vishal@Apples-MacBook-Pro.local>
2016-11-21 16:33:09 +0000

    second commit

---
+++ the_taz/urls.py
@@ -0,0 +1,36 @@
+"""the_taz URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from accounts import views as accounts_views
+from taz.views import get_index, get_menu,get_reservation,get_voucher,get_contact,get_opening,get_location
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', get_index, name='index'),
+    url(r'menu/', get_menu, name='menu'),
+    url(r'reservation/', get_reservation, name='reservation'),
+    url(r'voucher/', get_voucher, name='voucher'),
+    url(r'contact/', get_contact, name='contact'),
+    url(r'opening/', get_opening, name='opening'),
+    url(r'location/', get_location, name='location'),
+    url(r'^register/$', accounts_views.register, name='register'),
+    url(r'^profile/$', accounts_views.profile, name='profile'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
+    url(r'^cancel_subscription/$', accounts_views.cancel_subscription, name='cancel_subscription'),
+
+]
