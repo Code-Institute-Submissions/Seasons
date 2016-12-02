59131c91fa1ca7bcd2eabc65164e6370bbe25371
vishaldara <vishaldara@yahoo.com>
2016-12-02 12:00:14 +0000

    updated blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
-
 from django.conf.urls import url
 import views
 from django.views.static import serve
 from the_taz.settings import MEDIA_ROOT
 
 urlpatterns = [
-    url(r'^/', views.post_list),
+    url(r'^/$', views.post_list),
     url(r'^/(?P<id>\d+)/$', views.post_detail),
     url(r'^/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,13 @@ from .models import Post
 
 
 def post_list(request):
-
     posts = Post.objects.filter(published_date__lte=timezone.now()
                                 ).order_by('-published_date')
     return render(request, 'blogposts.html', {'posts': posts})
 
 
 def post_detail(request, id):
-    post  = get_object_or_404(Post, pk=id)
-    post.views += 1 # clock up the number of post views
+    post = get_object_or_404(Post, pk=id)
+    post.views += 1  # clock up the number of post views
     post.save()
-    return render(request, "postdetails.html",{'post': post})
+    return render(request, "postdetails.html", {'post': post})
