b29e8d807e24d8462ed7ef1e47c815441de23c3b
vishaldara <vishaldara@yahoo.com>
2016-12-02 13:36:47 +0000

    blog url post_list

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from the_taz.settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^/$', views.post_list, name="blog"),
+    url(r'^/(?P<id>\d+)/$', views.post_list),
     url(r'^/(?P<id>\d+)/$', views.post_detail),
     url(r'^/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 
